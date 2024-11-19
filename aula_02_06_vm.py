992fdb1ff2b7cdf9d4f610ea34b0abdd3a85f37d
fabiogomeess <fabiogomeesss@gmail.com>
2024-11-18 21:58:13 -0300

    Add files via upload

--- aula_02_06_vm.py
+++ aula_02_06_vm.py
@@ -1,1 +1,5 @@
 # Programa Velidade Média
+DP = float(input("Distância Percorrida(KM):"))
+Tempo = float(input("Tempo de Viagem(HORA):"))
+VM = DP/Tempo
+print (f"A velocidade média percorrida pelo veículo foi de {VM} km/h.")
--- aula_02_07_combustivel.py
+++ aula_02_07_combustivel.py
@@ -1,1 +1,5 @@
 # Programa Combustível Gasto
+KM_Percorrido = float(input("Informe quantos KM foram percorrido:"))
+Consumo_KM_Litro = 12
+Gasto = KM_Percorrido/Consumo_KM_Litro
+print (f"O veículo consumiu {Gasto:.1f} litros durante a viagem.")
